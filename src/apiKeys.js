62e586bc0c48cdd3707b2a7da6b49e6839694d4e
alka-tiwari <98600639+alka-tiwari@users.noreply.github.com>
2024-01-06 15:35:21 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "2f1dfd6c27f64e5690a55f38e8192587",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
---
+++ src/forcast.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import apiKeys from "./apiKeys";
+import ReactAnimatedWeather from "react-animated-weather";
+
+function Forcast() {
+  const [query, setQuery] = useState("");
+  const [error, setError] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (city) => {
+    axios
+      .get(
+        `${apiKeys.base}weather?q=${
+          city != "[object Object]" ? city : query
+        }&units=metric&APPID=${apiKeys.key}`
+      )
+      .then((response) => {
+        setWeather(response.data);
+        setQuery("");
+      })
+      .catch(function (error) {
+        console.log(error);
+        setWeather({});
+        setQuery("");
+        setError({ message: "Not Found", query: query });
+      });
+  };
+
+  useEffect(() => {
+    search("Delhi");
+  }, []);
+
+  return (
+    <div className="forecast">
+      <div className="forecast-icon">
+        <ReactAnimatedWeather
+          icon={weather.weather?.[0]?.icon || "CLEAR_DAY"}
+          color="white"
+          size={112}
+          animate={true}
+        />
+      </div>
+      <div className="today-weather">
+        <h3>{weather.weather?.[0]?.main}</h3>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search any city"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+          />
+          <div className="img-box">
+            <img
+              src="https://images.avishkaar.cc/workflow/newhp/search-white.png"
+              alt="Search"
+              onClick={() => search(query)}
+            />
+          </div>
+        </div>
+        <ul>
+          {weather.main ? (
+            <div>
+              <li className="cityHead">
+                <p>
+                  {weather.name}, {weather.sys.country}
+                </p>
+                <img
+                  className="temp"
+                  src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+                  alt="Weather Icon"
+                />
+              </li>
+              <li>
+                Temperature{" "}
+                <span className="temp">
+                  {Math.round(weather.main.temp)}°c ({weather.weather[0].main})
+                </span>
+              </li>
+              <li>
+                Humidity <span className="temp">{Math.round(weather.main.humidity)}%</span>
+              </li>
+              <li>
+                Visibility <span className="temp">{Math.round(weather.visibility)} mi</span>
+              </li>
+              <li>
+                Wind Speed <span className="temp">{Math.round(weather.wind.speed)} Km/h</span>
+              </li>
+            </div>
+          ) : (
+            <li>
+              {error.query} {error.message}
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Forcast;
